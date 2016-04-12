89c1e1f74514a27cf0e3e9d7679e7fe4f6638c78
Didier Sampaolo <didier@didcode.com>
2016-04-12 16:38:23 +0200

    works better in "prod" mode

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require_once('config.php');
 
 require __DIR__ . '/vendor/autoload.php';
 
-$api = new Soumettre\SoumettreApi('test');
+//$api = new Soumettre\SoumettreApi('test');
 $api = new Soumettre\SoumettreApi();
 
